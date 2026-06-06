c3f8c3cd2f069161b216b3e3181f10afcb869471
aiwao <233898056+aiwao@users.noreply.github.com>
2026-06-06 18:18:34 +0900

    init

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            gofumpt
+            golangci-lint
+            gotests
+            gopls
+            gotools
+
+            nodejs_latest
+            pnpm
+            vscode-langservers-extracted
+            vscode-css-languageserver
+          ];
+        };
+      }
+    );
+}
